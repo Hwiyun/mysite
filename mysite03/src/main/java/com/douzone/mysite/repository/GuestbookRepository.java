6933aa6403f9976cd544576be4cb196c3b5a393b
Hwiyun <sarakain123@gmail.com>
2023-03-02 14:54:53 +0900

    guestbook-spa

--- mysite03/src/main/java/com/douzone/mysite/repository/GuestbookRepository.java
+++ mysite03/src/main/java/com/douzone/mysite/repository/GuestbookRepository.java
@@ -15,10 +15,10 @@ public class GuestbookRepository {
 	@Autowired
 	private SqlSession sqlSession;
 	
-	public void deleteByNoAndPassword(Long no, String password) {
+	public int deleteByNoAndPassword(Long no, String password) {
 //		int[] a = {10, 20, 30};
 		Map<String, Object> map = Map.of("no", no, "password", password);
-		sqlSession.delete("guestbook.deleteByNoAndPassword", map);
+		return sqlSession.delete("guestbook.deleteByNoAndPassword", map);
 	}
 	
 	public void insert(GuestbookVo vo) {
